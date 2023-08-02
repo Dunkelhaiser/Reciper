50e9f38f10cff034f82307280ee9e03df8deae13
KaeserOfHonour <kaeser418@gmail.com>
2023-08-02 22:19:23 +0300

    Added auth metadata

---
+++ src/app/(auth)/sign_in/SignIn.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { redirect, useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import Button from "@components/Button";
+import Input from "@components/Input";
+import PasswordField from "@components/PasswordField";
+import { SignInForm, schema } from "@models/schemes/SignIn";
+import Form from "@components/Form";
+
+const SignIn = () => {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("callbackUrl") ?? "/";
+    const session = useSession();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm<SignInForm>({ resolver: zodResolver(schema), mode: "onBlur" });
+
+    const signInHandler = async (data: SignInForm) => {
+        const res = await signIn("credentials", { ...data, callbackUrl, redirect: false });
+        return res;
+    };
+
+    const { mutate, isLoading } = useMutation({
+        mutationFn: (data: SignInForm) => signInHandler(data),
+        onSuccess(data) {
+            if (data?.error) {
+                toast.error("Invalid credentials");
+            } else {
+                toast.success("Signed in successfully");
+            }
+        },
+        onError() {
+            toast.error("Failed to sign in");
+        },
+    });
+
+    if (session.status === "authenticated") return redirect("/");
+
+    return (
+        <Form label="Sign In" onSubmit={handleSubmit((data) => mutate(data))}>
+            <Button
+                label="Google"
+                onClick={() => signIn("google")}
+                className="mt-2 w-full bg-sky-600 hover:bg-sky-500 focus-visible:bg-sky-500 active:bg-sky-600"
+            />
+            <p className="mb-1 mt-3 text-center text-lg font-medium uppercase">or</p>
+            <div className="mb-2 flex flex-col gap-2 md:mb-4">
+                <Input name="email" placeholder="Email" register={register} errors={errors.email} />
+                <PasswordField name="password" placeholder="Password" register={register} errors={errors.password} />
+            </div>
+            <Button label="Sign In" className="mt-4 w-full" type="submit" disabled={!isValid || isLoading} />
+            <Link
+                href="/sign_up"
+                className="mt-4 block text-center text-orange-400 transition hover:text-orange-300 focus-visible:text-orange-300 active:text-orange-400"
+            >
+                Don&apos;t have an account? Sign up now!
+            </Link>
+        </Form>
+    );
+};
+
+export default SignIn;
---
+++ src/app/(auth)/sign_up/SignUp.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast/headless";
+import axios from "axios";
+import Button from "@components/Button";
+import Input from "@components/Input";
+import PasswordField from "@components/PasswordField";
+import { SignUpForm, schema } from "@models/schemes/SignUp";
+import Form from "@components/Form";
+
+const SignUp = () => {
+    const session = useSession();
+    const router = useRouter();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+    } = useForm<SignUpForm>({ resolver: zodResolver(schema), mode: "onBlur" });
+
+    const signUp = async (data: SignUpForm) => {
+        const res = await axios.post("/api/auth/sign_up", data);
+        return res.data;
+    };
+
+    const { mutate, isLoading } = useMutation({
+        mutationFn: (data: SignUpForm) => signUp(data),
+        onSuccess() {
+            toast.success("Signed up successfully");
+            router.push("/sign_in");
+        },
+        onError(err) {
+            if (axios.isAxiosError(err)) {
+                toast.error(err.response?.data.message);
+            } else {
+                toast.error("Failed to sign up");
+            }
+        },
+    });
+
+    if (session.status === "authenticated") return redirect("/");
+
+    return (
+        <Form label="Sign Up" onSubmit={handleSubmit((data) => mutate(data))}>
+            <div className="mb-2 flex flex-col gap-2 md:mb-4">
+                <Input name="username" placeholder="Username" register={register} errors={errors.username} />
+                <Input name="email" placeholder="Email" register={register} errors={errors.email} />
+                <PasswordField name="password" placeholder="Password" register={register} errors={errors.password} />
+                <PasswordField name="confirmPassword" placeholder="Confirm Password" register={register} errors={errors.confirmPassword} />
+            </div>
+            <Button label="Sign Up" className="mt-4 w-full" type="submit" disabled={!isValid || isLoading} />
+            <Link
+                href="/sign_in"
+                className="mt-4 block text-center text-orange-400 transition hover:text-orange-300 focus-visible:text-orange-300 active:text-orange-400"
+            >
+                Already have an account? Sign in now!
+            </Link>
+        </Form>
+    );
+};
+
+export default SignUp;
