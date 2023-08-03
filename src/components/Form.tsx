ea2652f22447130204d03110c9d0e09ea6ee90b4
KaeserOfHonour <103267805+KaeserOfHonour@users.noreply.github.com>
2023-08-03 23:55:45 +0300

    Fixed form layout

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Form = ({ label, children, onSubmit }: Props) => {
     return (
-        <form className="m-auto max-w-lg rounded-lg bg-white px-10 py-12 shadow dark:bg-stone-900 md:px-20" onSubmit={onSubmit}>
+        <form className="m-auto w-full max-w-lg rounded-lg bg-white px-10 py-12 shadow dark:bg-stone-900 md:px-20" onSubmit={onSubmit}>
             <h1 className="mb-4 text-4xl font-bold">{label}</h1>
             {children}
         </form>
