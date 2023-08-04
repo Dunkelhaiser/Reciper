eecee9ce7bf69e77e3e988750bcdb14a483798ce
KaeserOfHonour <103267805+KaeserOfHonour@users.noreply.github.com>
2023-08-04 20:59:59 +0300

    Added text gradient

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,8 @@ const Hero = () => {
         <section className="mx-auto grid gap-4 md:flex-grow md:grid-cols-2 md:items-center md:gap-8 xl:gap-20">
             <div>
                 <h1 className="block text-3xl font-bold text-stone-800 dark:text-stone-200 sm:text-4xl lg:text-6xl lg:leading-tight">
-                    Start your food journey with <span className="text-orange-400">Reciper</span>
+                    Start your food journey with{" "}
+                    <span className="bg-gradient-to-b from-orange-200 to-orange-400 bg-clip-text text-transparent">Reciper</span>
                 </h1>
                 <p className="mt-3 text-lg text-stone-600 dark:text-stone-300">Discover, create, and share delicious recipes.</p>
             </div>
